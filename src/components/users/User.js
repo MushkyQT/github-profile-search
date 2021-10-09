3bb05fbf17d6a9da9cb3ee71a1b89d641e6989bf
Charles <charlesmelki@gmail.com>
2021-10-09 14:40:31 +0200

    Refactored app level state to context API state

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,10 +1,14 @@
-import {useEffect} from "react";
+import {useEffect, useContext} from "react";
 import Spinner from "../layout/Spinner";
 import Repos from "../repos/Repos";
-import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
+import GithubContext from "../../context/github/githubContext";
 
-const User = ({user, loading, getUser, getUserRepos, repos, match}) => {
+const User = ({match}) => {
+  const githubContext = useContext(GithubContext)
+
+  const {getUser, getUserRepos, repos, loading, user} = githubContext
+
   useEffect(() => {
     getUser(match.params.login)
     getUserRepos(match.params.login)
@@ -79,14 +83,6 @@ const User = ({user, loading, getUser, getUserRepos, repos, match}) => {
       <Repos repos={repos}/>
     </>
   );
-}
-
-User.propTypes = {
-  loading: PropTypes.bool,
-  user: PropTypes.object.isRequired,
-  repos: PropTypes.array.isRequired,
-  getUser: PropTypes.func.isRequired,
-  getUserRepos: PropTypes.func.isRequired
 }
 
 export default User;
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,8 +1,13 @@
 import UserItem from "./UserItem";
 import Spinner from "../layout/Spinner";
-import PropTypes from "prop-types";
+import GithubContext from "../../context/github/githubContext";
+import {useContext} from "react";
 
-const Users = ({users, loading}) => {
+const Users = () => {
+  const githubContext = useContext(GithubContext)
+
+  const {users, loading} = githubContext
+
   if (loading) {
     return <Spinner/>
   } else {
@@ -14,11 +19,6 @@ const Users = ({users, loading}) => {
       </div>
     );
   }
-}
-
-Users.propTypes = {
-  users: PropTypes.array.isRequired,
-  loading: PropTypes.bool.isRequired
 }
 
 const userStyle = {
